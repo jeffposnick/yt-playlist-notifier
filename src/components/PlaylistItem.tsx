c1f58ec31c9435c91d7cac0940944e97fe39c1e0
Jeff Posnick <jeffy@google.com>
2022-01-10 16:49:15 -0500

    Notifications

--- src/components/PlaylistItem.tsx
+++ src/components/PlaylistItem.tsx
@@ -1,11 +1,14 @@
 import {getPlaylistItems, PlaylistSearch} from '../youtube';
 import {addSubscribedPlaylist, setPlaylistItems} from '../idb';
+import {requestPermission} from '../notifications';
 
 export function PlaylistItem(item: PlaylistSearch.Item) {
   const handleClick = async () => {
     await addSubscribedPlaylist(item.id.playlistId);
     const playlistItems = await getPlaylistItems(item.id.playlistId);
     await setPlaylistItems(item.id.playlistId, playlistItems);
+    const result = await requestPermission();
+    console.log(result);
   };
 
   return (
