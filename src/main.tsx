7d4eaf42854ecd4c918f74bc3fc9231eb130cc37
Jeff Posnick <jeffy@google.com>
2022-01-12 14:09:19 -0500

    Refactor if()

--- src/main.tsx
+++ src/main.tsx
@@ -20,32 +20,32 @@ if (import.meta.env.MODE === 'production') {
   window.addEventListener('load', () => {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js');
-    }
 
-    if (navigator.serviceWorker.controller) {
-      navigator.serviceWorker.controller.postMessage(UPDATE_CHECK);
-      navigator.serviceWorker.addEventListener('controllerchange', () => {
-        window.location.reload();
-      });
-    }
-
-    navigator.serviceWorker.ready.then(async (registration) => {
-      if ('periodicSync' in registration) {
-        const status = await navigator.permissions.query({
-          // @ts-expect-error
-          name: 'periodic-background-sync',
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller.postMessage(UPDATE_CHECK);
+        navigator.serviceWorker.addEventListener('controllerchange', () => {
+          window.location.reload();
         });
+      }
 
-        if (status.state === 'granted') {
-          await registration.periodicSync.register(UPDATE_CHECK, {
-            minInterval: 24 * 60 * 60 * 1000,
+      navigator.serviceWorker.ready.then(async (registration) => {
+        if ('periodicSync' in registration) {
+          const status = await navigator.permissions.query({
+            // @ts-expect-error
+            name: 'periodic-background-sync',
           });
+
+          if (status.state === 'granted') {
+            await registration.periodicSync.register(UPDATE_CHECK, {
+              minInterval: 24 * 60 * 60 * 1000,
+            });
+          } else {
+            navigator.serviceWorker.controller?.postMessage(UPDATE_CHECK);
+          }
         } else {
           navigator.serviceWorker.controller?.postMessage(UPDATE_CHECK);
         }
-      } else {
-        navigator.serviceWorker.controller?.postMessage(UPDATE_CHECK);
-      }
-    });
+      });
+    }
   });
 }
