2b064136d44c85176fa8142b2e861b40aa6e3232
Jeff Posnick <jeffy@google.com>
2022-01-11 19:51:36 -0500

    Absolute URL?

--- src/sw.ts
+++ src/sw.ts
@@ -32,7 +32,7 @@ async function showNotification(
   video: PlaylistItemList.Item,
 ) {
   self.registration.showNotification(video.snippet.title, {
-    badge: `/icon-192.png`,
+    badge: `${self.location.origin}/icon-192.png`,
     body: `A new video was added to '${playlistItem.snippet.title}'`,
     icon: video.snippet.thumbnails.high.url,
     tag: `https://youtu.be/${video.snippet.resourceId.videoId}`,
