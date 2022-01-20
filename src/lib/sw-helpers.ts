e4846cf6014b172caa95e2e5ce4950b2294a47e1
Jeff Posnick <jeffy@google.com>
2022-01-19 19:16:58 -0500

    Check for SW updates on visibility change

---
+++ src/lib/sw-helpers.ts
@@ -0,0 +1,53 @@
+import {UPDATE_CHECK} from '../constants';
+
+interface PeriodicSyncManager {
+  register(tag: string, options?: {minInterval: number}): Promise<void>;
+}
+
+declare global {
+  interface ServiceWorkerRegistration {
+    readonly periodicSync: PeriodicSyncManager;
+  }
+}
+
+export function initSW() {
+  window.addEventListener('load', () => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js');
+
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller.postMessage(UPDATE_CHECK);
+        navigator.serviceWorker.addEventListener('controllerchange', () => {
+          window.location.reload();
+        });
+      }
+
+      navigator.serviceWorker.ready.then(async (registration) => {
+        if ('periodicSync' in registration) {
+          const status = await navigator.permissions.query({
+            // @ts-expect-error
+            name: 'periodic-background-sync',
+          });
+
+          if (status.state === 'granted') {
+            await registration.periodicSync.register(UPDATE_CHECK, {
+              minInterval: 24 * 60 * 60 * 1000,
+            });
+          } else {
+            navigator.serviceWorker.controller?.postMessage(UPDATE_CHECK);
+          }
+        } else {
+          navigator.serviceWorker.controller?.postMessage(UPDATE_CHECK);
+        }
+
+        if (window.matchMedia('(display-mode: standalone)').matches) {
+          document.addEventListener('visibilitychange', () => {
+            if (document.visibilityState !== 'hidden') {
+              registration.update();
+            }
+          });
+        }
+      });
+    }
+  });
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,50 +2,10 @@ import {render} from 'preact';
 
 import './index.css';
 import {App} from './components/App';
-import {UPDATE_CHECK} from './constants';
+import {initSW} from './lib/sw-helpers';
 
 render(<App />, document.getElementById('app')!);
 
-interface PeriodicSyncManager {
-  register(tag: string, options?: {minInterval: number}): Promise<void>;
-}
-
-declare global {
-  interface ServiceWorkerRegistration {
-    readonly periodicSync: PeriodicSyncManager;
-  }
-}
-
 if (import.meta.env.MODE !== 'development') {
-  window.addEventListener('load', () => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/sw.js');
-
-      if (navigator.serviceWorker.controller) {
-        navigator.serviceWorker.controller.postMessage(UPDATE_CHECK);
-        navigator.serviceWorker.addEventListener('controllerchange', () => {
-          window.location.reload();
-        });
-      }
-
-      navigator.serviceWorker.ready.then(async (registration) => {
-        if ('periodicSync' in registration) {
-          const status = await navigator.permissions.query({
-            // @ts-expect-error
-            name: 'periodic-background-sync',
-          });
-
-          if (status.state === 'granted') {
-            await registration.periodicSync.register(UPDATE_CHECK, {
-              minInterval: 24 * 60 * 60 * 1000,
-            });
-          } else {
-            navigator.serviceWorker.controller?.postMessage(UPDATE_CHECK);
-          }
-        } else {
-          navigator.serviceWorker.controller?.postMessage(UPDATE_CHECK);
-        }
-      });
-    }
-  });
+  initSW();
 }
