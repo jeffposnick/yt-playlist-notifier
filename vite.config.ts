4dd856fbe6ec87eac5c0976c924c2c2f6323041e
Jeff Posnick <jeffy@google.com>
2022-01-24 14:12:42 -0500

    JIT props

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import {defineConfig} from 'vite';
 import preact from '@preact/preset-vite';
 import {VitePWA} from 'vite-plugin-pwa';
 import markdown, {Mode} from 'vite-plugin-markdown';
+import postcssJitProps from 'postcss-jit-props';
+import OpenProps from 'open-props';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [postcssJitProps(OpenProps)],
+    },
+  },
   plugins: [
     markdown({mode: [Mode.REACT]}),
     preact(),
