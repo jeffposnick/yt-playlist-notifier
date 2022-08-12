9c08334512765f76228543f4c6cb4bae41b46caa
Jeff Posnick <jeffy@google.com>
2022-08-12 11:39:32 -0400

    Tweaks

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,7 @@
 import type {PlaywrightTestConfig} from '@playwright/test';
 import {devices} from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const PORT = 3000;
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -13,13 +9,13 @@ import {devices} from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	testDir: './tests',
 	/* Maximum time one test can run for. */
-	timeout: 5 * 1000,
+	timeout: 30 * 1000,
 	expect: {
 		/**
 		 * Maximum time expect() should wait for the condition to be met.
 		 * For example in `await expect(locator).toHaveText();`
 		 */
-		timeout: 1000,
+		timeout: 10000,
 	},
 	/* Run tests in files in parallel */
 	fullyParallel: true,
@@ -36,8 +32,7 @@ const config: PlaywrightTestConfig = {
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
 		actionTimeout: 0,
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		// baseURL: 'http://localhost:3000',
-
+		baseURL: `http://localhost:${PORT}/`,
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
 	},
@@ -100,7 +95,7 @@ const config: PlaywrightTestConfig = {
 	/* Run your local dev server before starting the tests */
 	webServer: {
 		command: 'npm run stage',
-		port: 3000,
+		port: PORT,
 		reuseExistingServer: !process.env.CI,
 	},
 };
