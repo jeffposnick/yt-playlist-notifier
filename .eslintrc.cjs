fee8cd89c529247080d40fca4e3261a468359f1e
Jeff Posnick <jeffy@google.com>
2022-10-16 15:28:13 -0400

    More refactoring

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,6 +27,53 @@ module.exports = {
 				tsconfigRootDir: '.',
 				project: ['./tsconfig.json'],
 			},
+			plugins: ['react'],
+			rules: {
+				'react/no-deprecated': 2,
+				'react/react-in-jsx-scope': 0,
+				'react/display-name': [
+					1,
+					{
+						ignoreTranspilerName: false,
+					},
+				],
+				'react/jsx-no-bind': [
+					1,
+					{
+						ignoreRefs: true,
+						allowFunctions: true,
+						allowArrowFunctions: true,
+					},
+				],
+				'react/jsx-no-comment-textnodes': 2,
+				'react/jsx-no-duplicate-props': 2,
+				'react/jsx-no-target-blank': 2,
+				'react/jsx-no-undef': 2,
+				'react/jsx-tag-spacing': [
+					2,
+					{
+						beforeSelfClosing: 'always',
+					},
+				],
+				'react/jsx-uses-react': 2,
+				'react/jsx-uses-vars': 2,
+				'react/jsx-key': [
+					2,
+					{
+						checkFragmentShorthand: true,
+					},
+				],
+				'react/self-closing-comp': 2,
+				'react/prefer-es6-class': 2,
+				'react/prefer-stateless-function': 1,
+				'react/require-render-return': 2,
+				'react/no-danger': 1,
+				'react/no-did-mount-set-state': 2,
+				'react/no-did-update-set-state': 2,
+				'react/no-find-dom-node': 2,
+				'react/no-is-mounted': 2,
+				'react/no-string-refs': 2,
+			},
 		},
 		{
 			files: ['src/service-worker/**/*'],
@@ -52,54 +99,8 @@ module.exports = {
 		},
 	],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint', 'react'],
+	plugins: ['@typescript-eslint'],
 	root: true,
-	rules: {
-		'react/no-deprecated': 2,
-		'react/react-in-jsx-scope': 0,
-		'react/display-name': [
-			1,
-			{
-				ignoreTranspilerName: false,
-			},
-		],
-		'react/jsx-no-bind': [
-			1,
-			{
-				ignoreRefs: true,
-				allowFunctions: true,
-				allowArrowFunctions: true,
-			},
-		],
-		'react/jsx-no-comment-textnodes': 2,
-		'react/jsx-no-duplicate-props': 2,
-		'react/jsx-no-target-blank': 2,
-		'react/jsx-no-undef': 2,
-		'react/jsx-tag-spacing': [
-			2,
-			{
-				beforeSelfClosing: 'always',
-			},
-		],
-		'react/jsx-uses-react': 2,
-		'react/jsx-uses-vars': 2,
-		'react/jsx-key': [
-			2,
-			{
-				checkFragmentShorthand: true,
-			},
-		],
-		'react/self-closing-comp': 2,
-		'react/prefer-es6-class': 2,
-		'react/prefer-stateless-function': 1,
-		'react/require-render-return': 2,
-		'react/no-danger': 1,
-		'react/no-did-mount-set-state': 2,
-		'react/no-did-update-set-state': 2,
-		'react/no-find-dom-node': 2,
-		'react/no-is-mounted': 2,
-		'react/no-string-refs': 2,
-	},
 	settings: {
 		react: {version: '18'},
 	},
