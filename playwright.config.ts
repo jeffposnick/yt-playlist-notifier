064016db40c3857c58ebc7cab3e7fa373868a577
Jeff Posnick <github@posnick.org>
2026-09-03 00:30:52 -0400

    update playwright actions (#16)

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,10 +27,9 @@ const config: PlaywrightTestConfig = {
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
 	retries: 0,
-	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	workers: process.env.CI ? '50%' : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: [['dot'], ['html', {open: 'never'}]],
+	reporter: [['dot'], ['html', {open: 'never'}], ['github']],
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
